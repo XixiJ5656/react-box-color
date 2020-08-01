45b387d18aa2b6f99958350e54a3e8eae5c2aec1
sissy5656 <supersissy5656@gmail.com>
2020-08-01 13:26:32 -0500

    changeRandomColor

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    count: 0,
+    backgroundColor: "green",
+  };
+  // toggle = () => {
+  //   if (this.state.backgroundColor === "green") {
+  //     this.setState({ backgroundColor: "darkred" });
+  //   } else {
+  //     this.setState({ backgroundColor: "green" });
+  //   }
+  //   this.setState({ count: this.state.count + 1 });
+  // };
+
+  changeColor = () => {
+    const backgroundColor = [
+      "red",
+      "blue",
+      "green",
+      "lightgrey",
+      "lightgreen",
+      "purple",
+      "orange",
+      "pink",
+      "black",
+    ];
+    let randomColor =
+      backgroundColor[Math.floor(Math.random() * backgroundColor.length)];
+    this.setState({
+      count: this.state.count + 1,
+      backgroundColor: randomColor,
+    });
+  };
+  render() {
+    const pStyle = { fontSize: 25, color: "white", margin: "auto" };
+    const backgroundStyle = {
+      height: 300,
+      width: 300,
+      backgroundColor: this.state.backgroundColor,
+      display: "flex",
+    };
+    return (
+      <button
+        className="box"
+        onClick={this.changeColor}
+        style={backgroundStyle}
+      >
+        <p style={pStyle}>Click Here</p>
+        <p style={pStyle}>{this.state.count}</p>
+      </button>
+    );
+  }
 }
 
 export default App;
